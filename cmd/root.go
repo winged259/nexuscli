71ab45a6fd257987be2d7584204464bab2490bdc
duynk <duynk4@abbank.vn>
2025-04-25 17:06:09 +0700

    ends a day with no changes

--- cmd/root.go
+++ cmd/root.go
@@ -1,26 +1,33 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "nexuscli",
 	Short: "Nexus CLI",
 	Long:  "Nexus CLI tool to interact with nexus repository",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		println("hello")
 	},
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nexuscli.yaml)")
+
+	// rootCmd.AddCommand(authCmd)
+	// rootCmd.AddCommand(configCmd)
+	// rootCmd.AddCommand(repoCmd)
+	// rootCmd.AddCommand(repoDeleteCmd)
+	// rootCmd.AddCommand(repoListCmd)
 }
 
 func initConfig() {
@@ -28,14 +35,30 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Search config in home directory with name ".nexuscli" (without extension).
-		home, err := homedir.Dir()
+		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
 		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
 		viper.SetConfigName(".nexuscli")
 	}
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Can't read config:", err)
-		os.Exit(1)
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	notFound := &viper.ConfigFileNotFoundError{}
+
+	switch {
+	case err != nil && !errors.As(err, notFound):
+		cobra.CheckErr(err)
+	case err != nil && errors.As(err, notFound):
+		break
+
+	default:
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+
 	}
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
 }
